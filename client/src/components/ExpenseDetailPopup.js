68a023ba3568fc8c1354e90999610552300d1555
shengann <shengann2412@gmail.com>
2023-12-03 22:54:24 +0800

    ahndlechange for popup

--- client/src/components/ExpenseDetailPopup.js
+++ client/src/components/ExpenseDetailPopup.js
@@ -2,11 +2,37 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import FormRow from "./FormRow"
 import styled from 'styled-components';
+import moment from 'moment'
+import { useState } from 'react';
 
 const Section = styled.section`
 
 `
 const ExpenseDetailPopup = ({ showPopup, expenseDetails, onClose }) => {
+
+    const initialState = {
+        amount: expenseDetails.amount,
+        category: expenseDetails.category,
+        date: expenseDetails.date,
+        description: expenseDetails.description,
+        note: expenseDetails.note,
+    }
+
+    const [expenseData, setExpenseData] = useState(initialState);
+
+    const handleExpenseInput = (e) => {
+        const { name, value } = e.target;
+        setExpenseData((state) => ({
+            ...state,
+            [name]: value
+        }));
+
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    };
     return (
         <Section>
             <Modal
@@ -21,37 +47,43 @@ const ExpenseDetailPopup = ({ showPopup, expenseDetails, onClose }) => {
                         type="text"
                         name="date"
                         labelText='Date'
-                        value='sss'
+                        value={moment(expenseData.date).format("MMM Do, YYYY")}
+                        handleChange={handleExpenseInput}
                     />
                     <FormRow
                         type="text"
                         name="note"
                         labelText='Note'
-                        value='sss'
+                        value={expenseData.note}
+                        handleChange={handleExpenseInput}
                     />
                     <FormRow
                         type="text"
                         name="amount"
                         labelText='Amount'
-                        value='sss'
+                        value={expenseData.amount}
+                        handleChange={handleExpenseInput}
                     />
                     <FormRow
                         type="text"
                         name="category"
                         labelText='category'
-                        value='sss'
+                        value={expenseData.category}
+                        handleChange={handleExpenseInput}
                     />
                     <FormRow
                         type="text"
                         name="account"
                         labelText='Account'
-                        value='sss'
+                        value=''
+                        handleChange={handleExpenseInput}
                     />
                     <FormRow
                         name="description"
                         labelText='Description'
                         type="text"
-                        value='sss'
+                        value={expenseData.description}
+                        handleChange={handleExpenseInput}
                     />
 
                 </Modal.Body>
