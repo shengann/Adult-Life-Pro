0f8264eb9a718af8c1e509ac3a83bfab72b3bdbb
shengann <shengann2412@gmail.com>
2024-01-23 16:13:00 +0800

    remove unecessary stuff

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,8 +1,7 @@
 import Home from './Home'
 import Expenses from './Expenses'
-import FinancialReport from './FinancialReport'
 import AppLayout from './AppLayout'
 import ProtectedRoute from './ProtectedRoute'
 import Friends from './Friends'
 
-export { Home, Expenses, FinancialReport, AppLayout, ProtectedRoute, Friends }
+export { Home, Expenses, AppLayout, ProtectedRoute, Friends }
