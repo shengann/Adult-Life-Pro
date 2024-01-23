9c69d69358763e9b6e6e75bb3cea2f5cb118adfd
shengann <shengann2412@gmail.com>
2024-01-23 22:29:32 +0800

    add cors for backend deployemnt

--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ import ConnectDB from './config/connectDB.js'
 import ExpensesRouter from './routes/expensesRoute.js'
 import FriendsRouter from "./routes/friendsRoute.js"
 import cashFlowsRouter from "./routes/cashFlowsRoute.js"
+import cors from 'cors'; 
 
 dotenv.config()
 
 const port = process.env.PORT || 5000
 
 const app = express()
+app.use(cors({
+    origin: 'https://adult-life-pro.netlify.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+}));
 app.use(express.json())
 
 app.get('/', (req, res) => {
