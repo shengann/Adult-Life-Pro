2e8d79356ab12027719a8a707f5f46a0ff9227ec
shengann <shengann2412@gmail.com>
2024-01-23 23:24:27 +0800

    change baseurl

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: '',
+    baseUrl: 'https://adult-life-pro-server.onrender.com/',
 });
 
 export const apiSlice = createApi({
